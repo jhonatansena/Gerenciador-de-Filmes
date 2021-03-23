305816f87af96a9b7ce6068e1fc7f877a4c60253
Jhonatan Sena <jcostasenna@hotmail.com>
2021-03-23 17:02:15 -0300

    Feito o formulario de filtro

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { FilmesService } from 'src/app/core/filmes.service';
 import { Filme } from 'src/app/shared/models/filme';
 
@@ -12,9 +13,21 @@ export class ListagemFilmesComponent implements OnInit {
   filmes: Filme[] = [];
   pagina = 0;
   readonly qtdPagina = 4;
-  constructor(private filmeService: FilmesService) { }
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
+
+  constructor(private filmeService: FilmesService, private fb: FormBuilder) { }
 
   ngOnInit(): void{
+    this.filtrosListagem = this.fb.group(
+      {
+        texto: [''],
+        genero: ['']
+      }
+    );
+
+    this.generos = ['Ação', 'Romance', 'Aventura', 'Terror', 'Ficção cientifica', 'Comédia', 'Aventura', 'Drama'];
+
    this.listarFilmes();
 
   }
